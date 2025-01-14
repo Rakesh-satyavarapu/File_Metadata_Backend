62150ef6afbabef9ec61328a5e4a5fbe3024dd2e
Rakesh-satyavarapu <rakeshsatyavarapu369@gmail.com>
2025-01-14 16:14:08 +0530

    final commit

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+let multer = require('multer')
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
-
+let upload=multer({dest:'uploads'})
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse',upload.single('upfile'),(req,res)=>{
+  let {originalname,mimetype,size} = req.file
+  res.json({name:originalname,type:mimetype,size:size})
+})
 
+app.post('/')
 
 
 const port = process.env.PORT || 3000;
